a64aa0c576a18a28f3dce62dd1843072bb9455e4
Nidhi chauhan <sherlynidhi99@gmail.com>
2025-08-13 10:55:35 +0530

    58

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "%UNITY_WEBGL_BUILD_URL%");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/1.json");
 let WebXR;
 window.ARWT = {}
 
